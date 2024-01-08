a87dae80a7e427280d9ececef41bf4e50b530d3a
Akash_03 <akash.dange0307@gmail.com>
2024-01-08 13:29:21 +0530

    design layout updated part-1

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -41,14 +41,14 @@ function Todos() {
      { editform ? <form onSubmit={updateTodoHandler} className="space-x-3 mt-12">
         <input
           type="text"
-          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           placeholder="Enter a Todo..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
         <button
           type="submit"
-          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-3 w-[50%]"
         >
           Update Todo
         </button>
@@ -57,14 +57,14 @@ function Todos() {
       :  <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
         <input
           type="text"
-          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           placeholder="Enter a Todo..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
         <button
           type="submit"
-          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          className="text-white bg-indigo-500 border-0 py-2  focus:outline-none hover:bg-indigo-600 rounded text-lg mt-3 w-[50%]"
         >
           Add Todo
         </button>
